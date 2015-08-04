62f2c35a7b40aa45fa28a5b72972a596886f28ed
Jeka <jenyaslu@gmail.com>
2015-08-04 03:15:11 +0300

    Added tags support, displaying all notes data.

--- app/app.module.js
+++ app/app.module.js
@@ -1,4 +1,33 @@
 angular.module('app', [
-        'app.events',
-        'app.note'
-    ]);
+	]).controller('NoteCtrl', function($scope){
+		$scope.note = {
+			formatTags: function(){
+				if (this.tags != null) {
+					return this.tags.join(", ");
+				}
+			}
+		};
+		$scope.dataNote = {
+			id: '',
+			tags: [],
+			link: '',
+			timestamp: ''
+		};
+		$scope.notes = [];
+
+		$scope.saveNote = function() {
+			if ($scope.note.link == null){
+				return;
+			}
+			var date = new Date();
+			var tmpNote = $scope.note;			
+			$scope.dataNote = tmpNote;
+			if ($scope.note.tags != null){
+				$scope.dataNote.tags = tmpNote.tags.replace(" ", "").split(/[ ,]+/);
+			}
+			$scope.dataNote.timestamp = date;
+			$scope.notes.push($scope.dataNote);
+			$scope.note = {};
+		};
+
+	});

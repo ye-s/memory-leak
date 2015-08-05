702b95142ddfb9e29c0f3b8c1f703b35a64e7617
Jeka <jenyaslu@gmail.com>
2015-08-06 00:41:35 +0300

    Added app module for angular.

---
+++ app/app.module.js
@@ -0,0 +1,43 @@
+angular.module('app', [
+	]).controller('NoteCtrl', function($scope){
+		$scope.note = {
+			formatTags: function(){
+				if (this.tags != null) {
+					return this.tags.join(", ");
+				}
+			}
+		};
+		$scope.dataNote = {
+			id: '',
+			tags: [],
+			link: '',
+			info: '',
+			timestamp: ''
+		};
+		$scope.notes = [];
+
+		$scope.saveNote = function() {
+			if ($scope.note.link == null){
+				return;
+			}
+			var date = new Date();
+			var tmpNote = $scope.note;			
+			$scope.dataNote = tmpNote;
+			if ($scope.note.tags != null){
+				$scope.dataNote.tags = tmpNote.tags.replace(" ", "").split(/[ ,]+/);
+			}
+			$scope.dataNote.timestamp = date;
+			$scope.notes.push($scope.dataNote);
+			$scope.note = {};
+			addFormatTags();
+		};
+
+		addFormatTags = function() {
+			$scope.note.formatTags = function() {
+				if (this.tags != null) {
+					return this.tags.join(", ");
+				}
+			}
+		};
+
+	});
